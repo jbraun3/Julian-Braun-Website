77550dabdfa852970ffdd2f2fa55b832910c5099
Julian Anthony Braun <jbraun3@uw.edu>
2026-02-16 23:57:40 -0800

    make it mine

---
+++ Julian-Braun-Website/src/app/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './assets/Global.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>
+        Wow welcome to jules's website
+      </p>
+    </>
+  )
+}
+
+export default App
